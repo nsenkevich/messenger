0214e67986746c0c1b70e81130ee8bed4ab97794
Nikolai <nsenkevich@gmail.com>
2024-09-08 22:49:01 +0100

    firebase fix

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,14 +11,12 @@ import { provideRouter } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom(
-      provideFirebaseApp(() => initializeApp(environment.firebase)),
-      provideFirestore(() => getFirestore()),
-      provideAuth(() => getAuth()),
-      provideFunctions(() => getFunctions()),
-      provideStorage(() => getStorage()),
-      provideMessaging(() => getMessaging())
-    ),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+    provideAuth(() => getAuth()),
+    provideFunctions(() => getFunctions()),
+    provideStorage(() => getStorage()),
+    provideMessaging(() => getMessaging()),
     provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"messenger-826b9","appId":"1:227475481309:web:4aacfa942eb2b3db1ff134","storageBucket":"messenger-826b9.appspot.com","locationId":"europe-west2","apiKey":"AIzaSyAMaLLKSSvQOTaNcydkx5EfltFUQq0dfWM","authDomain":"messenger-826b9.firebaseapp.com","messagingSenderId":"227475481309","measurementId":"G-1BLCTW2WLW"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideFunctions(() => getFunctions()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())
   ],
 };
